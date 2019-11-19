e250a248965255cafcbab23307dbc49f75856379
heathdutton <hdutton@thedmsgrp.com>
2019-11-19 10:42:14 -0500

    Add DB logging in debug mode.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.debug')) {
+            DB::listen(function ($query) {
+                // Simulate ORM's treatment of integers/strings.
+                $bindings = [];
+                foreach ($query->bindings as $value) {
+                    if (is_int($value)) {
+                        $bindings[] = $value;
+                    } else {
+                        $bindings[] = "'".$value."'";
+                    }
+                }
+                $rawSql = vsprintf(str_replace('?', '%s', $query->sql), $bindings);
+                Log::debug('SQL: '.$rawSql);
+            });
+        }
     }
 }
